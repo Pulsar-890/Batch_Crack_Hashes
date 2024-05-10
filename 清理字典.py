59d6d5f74bd43991b2a2254a5a0cef9c1fb5f51d
Pulsar <2924917407@qq.com>
2024-05-10 15:29:46 +0800

    版本1.1.7 修复了两个未导入库的bug

--- 清理字典.py
+++ 清理字典.py
@@ -1,10 +1,12 @@
+from tqdm import tqdm
+
 def clear_dict(filename="www.txt",path=""):
     print(f"正在清理字典中的非法字符")
     with open(path+filename,"rb") as f:
         txtt=f.read().replace(b'\r',b'').split(b'\n')
         txta=list(set([txtt[j].decode('gbk',errors='ignore') for j in tqdm(range(len(txtt)),"清理非法字符")]))
     print("稍候片刻，正在保存...")
-    txt("w",txta,path+'new_'+filename)
+    open(path+'new_'+filename,"w").write("\n".join(txta))
     print(f"清理完成，字典{'new_'+filename}的长度为{len(txta)}")
     return txta
 
